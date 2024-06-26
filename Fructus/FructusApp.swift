6a5c771bc79d81ed54f1f7ca12122c493602cad7
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-25 22:50:37 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Oscar David Myerston Vega on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
